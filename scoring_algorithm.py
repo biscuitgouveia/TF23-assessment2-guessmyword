a30aa7bccaefc170dd2a8c5e75a75b680dbb3fe5
Ethan Gouveia <ethangouveiamain@gmail.com>
2023-03-12 13:19:37 +0800

    Created a simpler and cleaner scoring algorithm

--- scoring_algorithm.py
+++ scoring_algorithm.py
@@ -1,73 +1,48 @@
-import random
 
-MAX_TRIES = 6
-TARGET_WORDS = open("word-bank/all_words.txt")
-VALID_WORDS = open("word-bank/target_words.txt")
-attempts = 0
-game_state = "in_game"
+def score_guess(guess, target_word):
+    """given two strings of equal length, returns a tuple of ints representing the score of the guess
+    against the target word (MISS (0), MISPLACED (1), or EXACT (2)
+    Here are some examples (will run as doctest):
 
-def choose_word(word_list):
-    valid_words_list = word_list.readlines()
-    target_word = random.choice(valid_words_list)
-    return random.choice(target_word)
+    >>> score_guess('hello', 'hello')
+    (2, 2, 2, 2, 2)
+    >>> score_guess('drain', 'float')
+    (0, 0, 1, 0, 0)
+    >>> score_guess('hello', 'spams')
+    (0, 0, 0, 0, 0)
+    >>> score_guess('gauge', 'range')
+    (0, 2, 0, 2, 2)
+    >>> score_guess('melee', 'erect')
+    (0, 1, 0, 1, 0)
+    >>> score_guess('array', 'spray')
+    (0, 0, 2, 2, 2)
+    >>> score_guess('train', 'tenor')
+    (2, 1, 0, 0, 1)
+        """
 
-# TODO: Simplify the scoring algorithm. It works, but it's a bit convoluted
+    if guess == target_word:
+        return (2, 2, 2, 2, 2)
 
-def score_word(guess_word, target_word):
-
-    score_string = str()
-    score_list = list()
+    score_list = [0, 0, 0, 0, 0]
     target_frequency = dict()
     guess_frequency = dict()
-    green = "\033[0;32m"
-    yellow = "\033[0;33m"
-    white = "\033[0;37m"
 
-    for letter in guess_word:
-        score_list.append((letter, white))
-
-    for letter in target_word:
-        if letter not in target_frequency:
-            target_frequency[letter] = 1
-        else:
-            target_frequency[letter] += 1
-
-    for letter in guess_word:
-        guess_frequency[letter] = 0
-
-    for counter, letter in enumerate(guess_word):
-        if letter == target_word[counter]:
+    for word_index, letter in enumerate(guess):
+        target_frequency[target_word[word_index]] = target_word.count(target_word[word_index])
+        if letter not in guess_frequency:
+            guess_frequency[letter] = 0
+        if letter == target_word[word_index]:
+            score_list[word_index] = 2
             guess_frequency[letter] += 1
-            score_list[counter] = (letter, green)
+        elif letter not in target_word:
+            score_list[word_index] = 0
 
-    for counter, letter in enumerate(guess_word):
-        if score_list[counter][1] == green:
-            continu
-        else:
-            guess_frequency[letter] += 1
-        if letter in target_word and guess_frequency[letter] <= target_frequency[letter]:
-            score_list[counter] = (letter, yellow)
-        else:
-            score_list[counter] = (letter, white)
+    for word_index, letter in enumerate(guess):
+        if letter in target_word:
+            if score_list[word_index] == 2:
+                continue
+            elif guess_frequency[letter] < target_frequency[letter]:
+                score_list[word_index] = 1
+                guess_frequency[letter] += 1
 
-    for letter in score_list:
-        score_string += letter[1] + letter[0]
-
-    return score_string
-
-def validate_word(guess_word):
-    if len(guess_word) < 5:
-        return "Invalid - Not enough letters. Please enter a five letter English word: "
-    elif len(guess_word):
-        return "Invalid - Too many letters. Please enter a five letter English word: "
-    elif word not in ALL_WORDS.readlines():
-        return "Invalid - Word not found in the dictionary. Please try again: "
-    elif guess_word == target_word:
-        return "win"
-    else:
-        return True
-
-target_word = choose_word("word-bank/target_words.txt")
-guess_word = input("Type a five letter English word and press enter: ")
-
-while game_state == in_game:
+    return tuple(score_list)
